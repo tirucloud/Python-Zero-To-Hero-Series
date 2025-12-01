d3ce60749f966085c4040976ad28ea676f526cda
tirucloud <tirucloud@gmail.com>
2025-12-01 19:50:46 +0530

    all

---
+++ Day-1/list_tuple_set_dict.py
@@ -0,0 +1,24 @@
+# List
+x_list = ["apple", "banana", "cherry"]
+
+# Tuple
+x_tuple = ("apple", "banana", "cherry")
+
+# Set
+x_set = {"apple", "banana", "cherry"}
+
+# Dictionary
+x_dict = {"name": "John", "age": 36}
+
+print(x_list)
+print(type(x_list))
+
+print(x_tuple)
+print(type(x_tuple))
+
+print(x_set)
+print(type(x_set))
+
+print(x_dict)
+print(type(x_dict))
+
