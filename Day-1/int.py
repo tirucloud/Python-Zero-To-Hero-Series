89da6faa8a45b8a7c79759d4267c02dc551bd52d
tirucloud <tirucloud@gmail.com>
2025-12-02 18:13:09 +0530

    abc

---
+++ Day-1/int.py
@@ -0,0 +1,15 @@
+books = 5
+print(f"I have {books} books.")
+print(type(books))
+
+price = 19.99
+print(f"This book costs ${price}.")
+type(price)
+
+quantity = 3
+total_cost = price * quantity  # Let's calculate the total cost
+print(f"The total cost for {quantity} books is ${total_cost:.2f}.")
+
+name = "Alice"
+print(f"Hello, {name}!")
+type(name)
